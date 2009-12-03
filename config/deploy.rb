b7dc1a85a41aabe25e257a13dbee5442cdb0b45b
Jonas Höglund <jonas@stixy.com>
2009-12-03 14:11:13 +0100

    misc

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,3 +11,18 @@ set :deploy_to, "/home/clients/sites/#{client}/#{project}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 set :scm, :git
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
--- db/migrate/20091203103832_add_instructore_to_course.rb
+++ db/migrate/20091203103832_add_instructore_to_course.rb
@@ -1,11 +1,11 @@
 class AddInstructoreToCourse < ActiveRecord::Migration
   def self.up
-    add_column :courses, :instructore_id, :integer
+    add_column :courses, :instructor_id, :integer
     remove_column :courses, :teatcher
   end
 
   def self.down
-    remove_column :courses, :instructore_id
+    remove_column :courses, :instructor_id
     add_column :courses, :teatcher, :string
   end
 end
