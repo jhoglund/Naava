11166c6c36eafe8ab0aad7da05e3a3f5cc09c190
Jonas Höglund <jonas@stixy.com>
2009-12-03 13:19:31 +0100

    misc

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ default_run_options[:pty] = true
 
 set :client, "naava"
 set :project, "website"
-set :repository, "ssh://jonas@67.23.34.66/home/clients/work/#{client}/#{project}/application"
+set :repository, "git@github.com:jhoglund/Naava.git"
 set :ssh_options, {:forward_agent => true}
 
 set :deploy_to, "/home/clients/sites/#{client}/#{project}"
