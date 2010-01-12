ab9c27f01eeae16b8d9a98028c3474c79cc18f0c
Jonas Höglund <jonas@stixy.com>
2010-01-12 10:32:17 +0100

    added verify code for google

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ namespace :admin do
  
   desc "Crawl pages using the Mechanize gem. Set URL variable as a starting point. Set CREDS as username:password if you are hitting a password protected site. To generate a Google Sitemap in /public/sitemap.xml, set SITEMAP=true. To suppress output and only show errors, set QUIET=true. To show more details during output, set DEBUG=true."
   task :crawl_pages do
-    start_url = ENV["URL"] || "http://localhost:3000"
+    start_url = ENV["URL"] || "http://www.navayoga.se/"
     sitemap = Crawler.new(start_url, (ENV["CREDS"] if ENV["CREDS"]), ENV["QUIET"] || false, ENV["SITEMAP"] || false, ENV["DEBUG"] || false)
   end
  
