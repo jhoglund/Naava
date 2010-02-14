47cf69507ff0f254fb36adc402172a447b357295
Jonas Höglund <jonas@stixy.com>
2010-02-14 15:54:24 +0100

    added admin booking

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,8 @@
 class Participant < ActiveRecord::Base
   has_many :bookings, :dependent => :destroy
   has_many :attendants, :dependent => :destroy 
+  
+  def attending?
+    !attendants.attending.blank?
+  end
 end
