5b6a9cc5b61ef73888bd7fd69006dfcf3c0a6d61
Jonas Höglund <jonas@stixy.com>
2010-08-02 16:59:53 +0200

    added fullCalendar

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -8,7 +8,7 @@ module CoursesHelper
           var clone = template.cloneNode(true);
           template.parentNode.appendChild(clone)
           var elements = clone.getElementsByTagName('*')
-          $(elements).each(function(element){
+          $(elements).each(function(index,element){
             if(element.name){
               element.name = element.name.replace(/(course\[sessions_attributes\]\[)(\d+)(\])/,function(str,start,num,end){ return start + (parseInt(num)+1) + end })
             }
