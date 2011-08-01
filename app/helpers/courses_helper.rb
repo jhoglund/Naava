43e8aeadac5dd8acb2111588824d39c1672b2023
Jonas Höglund <jonas@stixy.com>
2011-08-01 13:43:52 +0200

    Automatically set date of new session to 7 days later than last session when creating new session

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -11,6 +11,16 @@ module CoursesHelper
           $(elements).each(function(index,element){
             if(element.name){
               element.name = element.name.replace(/(course\[sessions_attributes\]\[)(\d+)(\])/,function(str,start,num,end){ return start + (parseInt(num)+1) + end })
+              var el = $(template).find('select[id*="starts_at"]');
+              var date = new Date($(el[2]).val(),$(el[1]).val()-1,$(el[0]).val(),$(el[3]).val(),$(el[4]).val());
+              date.setDate(date.getDate()+7);
+              var clone_el = $(clone).find('select[id*="starts_at"]');
+              $(clone_el[2]).val(date.getFullYear());
+              $(clone_el[1]).val(date.getMonth()+1);
+              $(clone_el[0]).val(date.getDate());
+              // Needs to always be two digits long
+              //$(clone_el[3]).val(date.getHours());
+              //$(clone_el[4]).val(date.getMinutes());
             }
           })
         })()
@@ -18,3 +28,4 @@ module CoursesHelper
     end
   end  
 end
+
