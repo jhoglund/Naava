a8476717317434ea6c9beee3fe775a6581ed72ac
Jonas Höglund <jonas@stixy.com>
2011-12-29 07:37:30 +0100

    Converted erb to haml

--- config/environment.rb
+++ config/environment.rb
@@ -20,9 +20,14 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem 'i18n' , :version => '0.3.7'
   config.gem 'mechanize'
+  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  
+  # haml
   config.gem 'haml'
   config.gem 'sass'
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  config.gem 'hpricot'
+  config.gem 'erubis'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
